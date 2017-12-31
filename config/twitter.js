802528410d75aa4f96df564b487db7a9c66b8252
Henrique Borges <henriquelb96@gmail.com>
2017-12-31 20:53:16 -0200

    changes

--- config/twitter.js
+++ config/twitter.js
@@ -14,7 +14,3 @@ client.get('statuses/user_timeline', params, function (error, tweets, response) 
     }
 });
 module.exports = client;
-/*consumer_key: '54JnaDU5EJvasU5gwyKsLIP6V',
-consumer_secret: 'DAu6d4kXwxe2qVrRYb7RMaEnONQzPgq0PnqwvwUjPPs6OZktzm',
-access_token_key: '4512577469-KzyN5twu8WkfNggDp58b37QMj93yJWGofOHXFpV',
-access_token_secret: 'bLiub9Dwwa0iIvdt5AjNIhpFLJI7aqk4ONhknhey45yCw'*/
